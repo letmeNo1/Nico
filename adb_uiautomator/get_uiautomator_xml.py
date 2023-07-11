0ff193ce0ba8f16daf487889fae55d6b3367ec5a
DESKTOP-L3RQRF1\swtest <hanhuang@jabra.com>
2023-07-11 15:28:45 +0800

    no message

--- adb_uiautomator/get_uiautomator_xml.py
+++ adb_uiautomator/get_uiautomator_xml.py
@@ -4,23 +4,45 @@ import tempfile
 
 from lxml import etree
 
+from adb_uiautomator import logger_config
+from adb_uiautomator.logger_config import logger
+
 
 class UIStructureError(Exception):
     pass
 
 
+def check_file_exists_in_sdcard(udid, file_name):
+    rst = os.popen(f"adb -s {udid} shell ls {file_name}")
+    return True if rst.read().find("No such file or directory") > 0 else False
+
+
+def init_adb_auto(udid):
+    start_auto_name = "sdcard/start_auto"
+    uiautomator_jar_name = "sdcard/bin/uiautomator.jar"
+    lib_path = os.path.dirname(__file__) + "\lib"
+    if not check_file_exists_in_sdcard(udid, start_auto_name):
+        start_auto_file_path = lib_path + "\\start_auto"
+        os.popen(f"adb -s {udid} push {start_auto_file_path} sdcard")
+    if not check_file_exists_in_sdcard(udid, uiautomator_jar_name):
+        uiautomator_jaro_file_path = lib_path + "\\uiautomator.jar"
+        os.popen(f"adb -s {udid} push {uiautomator_jaro_file_path} sdcard/bin")
+
+
 def get_ui_xml(udid, reload):
     if check_xml_exists(udid) and reload is False:
         rst = "exists"
-        print("already exit")
+        logger.debug("already exit")
     else:
-        commands = """
+        commands = f"""
                  su
                  cd /data/local/
                  mkdir tmp
-                 uiautomator dump --compressed /data/local/tmp/%s_ui.xml
-                 """ % udid
-
+                 cd ..
+                 cd ..
+                 cd /sdcard
+                 /system/bin/sh start_auto dump --compressed /data/local/tmp/{udid}_ui.xml
+                 """
         adb_process = subprocess.Popen("adb -s %s shell" % udid, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE, text=True)
         adb_process.stdin.write(commands)
@@ -28,14 +50,13 @@ def get_ui_xml(udid, reload):
         rst, error = adb_process.communicate()
         if rst == "":
             raise UIStructureError("Failed to obtain UI structure")
-    print("adb uiautomator was initialized successfully")
+    logger.debug("adb uiautomator was initialized successfully")
     return rst
 
 
 def check_xml_exists(udid):
     temp_folder = tempfile.gettempdir()
     path = temp_folder + f"\\{udid}_ui.xml"
-    print(os.path.exists(path))
     return os.path.exists(path)
 
 
@@ -49,11 +70,10 @@ def remove_xml(udid):
 def pull_ui_xml_to_temp_dir(udid):
     temp_folder = tempfile.gettempdir()
     get_root_cmd = f"adb -s {udid} root"
-    rst = os.popen(get_root_cmd).read()
-    print(rst)
+    os.popen(get_root_cmd).read()
     command = f'adb -s {udid} pull /data/local/tmp/{udid}_ui.xml {temp_folder}'
     rst = os.popen(command).read()
-    print(rst)
+    logger.debug(rst)
     return temp_folder + f"\\{udid}_ui.xml"
 
 
@@ -74,7 +94,7 @@ def get_root_element(udid, reload=False):
             get_ui_xml(udid, reload)
             break
         except UIStructureError:
-            print(f"init fail, retry {i} times")
+            logger.debug(f"init fail, retry {i} times")
 
     xml_file_path = pull_ui_xml_to_temp_dir(udid)
     # 解析XML文件
--- adb_uiautomator/nico.py
+++ adb_uiautomator/nico.py
@@ -1,12 +1,11 @@
+from adb_uiautomator.logger_config import logger
 from adb_uiautomator.nico_proxy import NicoProxy
 
-from adb_uiautomator.get_uiautomator_xml import get_root_element, remove_xml
+from adb_uiautomator.get_uiautomator_xml import get_root_element, remove_xml, init_adb_auto
 from adb_uiautomator.utils import Utils
-
 
 class UIStructureError(Exception):
     pass
-
 
 
 class AdbAutoNico(Utils):
@@ -14,7 +13,10 @@ class AdbAutoNico(Utils):
         super().__init__(udid)
         self.udid = udid
         remove_xml(self.udid)
+        init_adb_auto(self.udid)
 
     def __call__(self, **query):
+        logger.debug("no found, try again")
+
         root = get_root_element(self.udid)
         return NicoProxy(root, self.udid, **query)
