e4979788f85ed2cb01d52f914c618cd76daf091c
hank.huang <44250052+letmeNo1@users.noreply.github.com>
2023-08-02 15:49:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 import os
 
-from adb_uiautomator.nico import AdbAutoNico
+from nico.nico import AdbAutoNico
+from nico.utils import Utils
 
 nico = AdbAutoNico("514f465834593398")
-nico.start_app("com.android.settings/.Settings")
+utils = Utils("514f465834593398")
+utils.start_app("com.android.settings/.Settings")
 Link = nico(text ="连接").wait_for_appearance()
 Link.click()
 WLAN = nico(text = "WLAN").wait_for_appearance()
 WLAN.click()
-nico.back()
+utils.back()
 bluetooth = nico(text="蓝牙").wait_for_appearance()
 bluetooth.click()
-nico.back()
+utils.back()
 
 
 os.system("adb shell am instrument -w -r -e class com.example.myapplication.HierarchyTest com.example.myapplication.test/androidx.test.runner.AndroidJUnitRunner")
